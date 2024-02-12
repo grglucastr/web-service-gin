bccd56becd5b3844e0d0f6671c0018a7a6a5c829
= <george.bentes@gmail.com>
2024-02-12 16:23:18 -0300

    add: get album by id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,21 +19,21 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func main(){
+func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
 
-
-func getAlbums (c *gin.Context){
+func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func postAlbums (c *gin.Context){
-	var newAlbum album 
+func postAlbums(c *gin.Context) {
+	var newAlbum album
 
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
@@ -40,4 +41,18 @@ func postAlbums (c *gin.Context){
 
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusOK, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, album := range albums {
+		for album.ID == id {
+			c.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found"})
+
 }
